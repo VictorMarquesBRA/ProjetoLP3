8b1318ecfa40cd9cd39090d2923095ffdfce2718
31507239 <31507239@087996-P33S103.macklabs.br>
2017-05-04 10:33:53 -0300

    Criação da classe NasaAPOD e do servlet NasaAPOD

---
+++ NABRASA/NABRASA-war/src/java/com/nabrasa/persistence/entities/NasaAPOD.java
@@ -0,0 +1,95 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.nabrasa.persistence.entities;
+
+import java.util.Date;
+
+/**
+ *
+ * @author 31507239
+ */
+
+//Classe que representa a imagem do dia (Nasa APOD)
+public class NasaAPOD {
+    
+    private Date date;
+    private String explanation;
+    private String hdurl;
+    private String media_type;
+    private String service_version;
+    private String title;
+    private String url;
+
+    public NasaAPOD() {
+    }
+
+    public NasaAPOD(Date date, String explanation, String hdurl, String media_type, String service_version, String title, String url) {
+        this.date = date;
+        this.explanation = explanation;
+        this.hdurl = hdurl;
+        this.media_type = media_type;
+        this.service_version = service_version;
+        this.title = title;
+        this.url = url;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getExplanation() {
+        return explanation;
+    }
+
+    public void setExplanation(String explanation) {
+        this.explanation = explanation;
+    }
+
+    public String getHdurl() {
+        return hdurl;
+    }
+
+    public void setHdurl(String hdurl) {
+        this.hdurl = hdurl;
+    }
+
+    public String getMedia_type() {
+        return media_type;
+    }
+
+    public void setMedia_type(String media_type) {
+        this.media_type = media_type;
+    }
+
+    public String getService_version() {
+        return service_version;
+    }
+
+    public void setService_version(String service_version) {
+        this.service_version = service_version;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+    
+}
